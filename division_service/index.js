9f78d3fcb32f926d3afd0c8199049cda2e91822d
Youssef Negm <usf.ngm@gmail.com>
2020-01-29 20:19:11 +0200

    port fix

--- division_service/index.js
+++ division_service/index.js
@@ -41,10 +41,10 @@ app.use(function fiveHundredHandler(err, req, res, next) {
 
 
 // Start server
-app.listen(3000, function (err) {
+app.listen(8080, function (err) {
   if (err) {
     return console.error(err)
   }
 
-  console.log('Started at http://localhost:3000')
+  console.log('Started at http://localhost:8080')
 })
